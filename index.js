8f0032bd9f76771e1b48af545570e6cd28172a8c
cspdevs000 <cspdevs000@gmail.com>
2021-11-16 20:06:55 -0500

    playing in class

---
+++ index.js
@@ -0,0 +1,6 @@
+const faker = require('faker');
+const axios = require('axios');
+const newModule = require('./newModule.js');
+
+console.log(newModule);
+console.log(newModule.addNumber(1,2));
---
+++ newModule.js
@@ -0,0 +1,14 @@
+function addNumber(num1,num2) {
+   return num1 + num2;
+}
+
+const person = {
+    name: "me",
+    age: 33,
+    foods: ['soylent', 'pizza'],
+}
+
+module.exports = {
+    person,
+    addNumber
+}
